f1f9503d98d6d54ce24ce68c6109a3d00f023d50
CPenalba <carolinapenalbacorpa@gmail.com>
2025-04-02 14:13:48 +0200

    Terminado

--- ApiAzureStorageTokens/Program.cs
+++ ApiAzureStorageTokens/Program.cs
@@ -1,52 +1,39 @@
+using ApiAzureStorageTokens.Services;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
+builder.Services.AddTransient<ServiceSaSToken>();
 // Learn more about configuring OpenAPI at https://aka.ms/aspnet/openapi
 builder.Services.AddOpenApi();
 
 var app = builder.Build();
 
 // Configure the HTTP request pipeline.
-if (app.Environment.IsDevelopment())
-{
-    
-}
-app.MapOpenApi();
-app.UseSwaggerUI(options =>
-{
-    options.SwaggerEndpoint(url: "/swagger/v1/swagger.json", name: "Api Azure Storage Tokens");
-    options.RoutePrefix = "";
+if (app.Environment.IsDevelopment()){
+
+}app.MapOpenApi();app.UseSwaggerUI(options => {    options.SwaggerEndpoint("/openapi/v1.json", "Azure Minimal Api");    options.RoutePrefix = "";
+});app.UseHttpsRedirection();
+
+//app.MapGet("/testing", () =>
+//{
+//    return "sin nada";
+//});
+
+//app.MapGet("/parametros/{dato}", (string dato
+//    , ObjetoInyectado service) =>
+//{
+//    return "con algo: " + dato;
+//});
+
+//NECESITAMOS UN METODO GET PARA ACCEDER AL TOKEN MEDIANTE EL CURSO
+//EL METODO, AL DECLARARLO, LLEVA EL Routing DE ACCESO Y TAMBIEN 
+//PUEDE LLEVAR PARAMETROS
+//NECESITAMOS RECUPERAR EL SERVICE DENTRO DE NUESTRO METODO 
+//DEL MINIMAL API
+app.MapGet("/token/{curso}", (string curso
+, ServiceSaSToken service) =>{    string token = service.GenerateToken(curso);
+    return new { token = token };
 });
 
-app.UseHttpsRedirection();
-
-var summaries = new[]
-{
-    "Freezing", "Bracing", "Chilly", "Cool", "Mild", "Warm", "Balmy", "Hot", "Sweltering", "Scorching"
-};
-
-app.MapGet("/otro", () =>
-{
-    return "Hola mundo de miercoles!!!";
-});
-
-app.MapGet("/weatherforecast", () =>
-{
-    var forecast = Enumerable.Range(1, 5).Select(index =>
-        new WeatherForecast
-        (
-            DateOnly.FromDateTime(DateTime.Now.AddDays(index)),
-            Random.Shared.Next(-20, 55),
-            summaries[Random.Shared.Next(summaries.Length)]
-        ))
-        .ToArray();
-    return forecast;
-})
-.WithName("GetWeatherForecast");
-
-app.Run();
-
-internal record WeatherForecast(DateOnly Date, int TemperatureC, string? Summary)
-{
-    public int TemperatureF => 32 + (int)(TemperatureC / 0.5556);
-}
+app.Run();
---
+++ ApiAzureStorageTokens/Services/ServiceSaSToken.cs
@@ -0,0 +1,37 @@
+﻿using Azure.Data.Tables.Sas;
+using Azure.Data.Tables;
+
+namespace ApiAzureStorageTokens.Services
+{
+    public class ServiceSaSToken
+    {
+        private TableClient tablaAlumnos;
+
+        public ServiceSaSToken(IConfiguration configuration)
+        {
+            string azureKeys = configuration.GetValue<string>("AzureKeys:StorageAccount");
+            TableServiceClient serviceClient = new TableServiceClient(azureKeys);
+            this.tablaAlumnos =serviceClient.GetTableClient("alumnos");
+        }
+
+        public string GenerateToken(string curso)
+        {
+            //NECESITAMOS LOS PERMISOS DE ACCESO, POR AHORA SOLAMNETE PERMITIREMOS LECTURA
+            TableSasPermissions permisos = TableSasPermissions.Read | TableSasPermissions.Add;
+
+            //EL ACCESO AL TOKEN ESTA DELIMITADO POR UN TIEMPO
+            //NECESITAMOS INCLUIR EL TIEMPO QUE TENDRA ACCESO A LEER LOS ELEMENTOS
+            TableSasBuilder builder = this.tablaAlumnos.GetSasBuilder(permisos, DateTime.UtcNow.AddMinutes(30));
+
+            //COMO ROW KEY Y PARTITION KEY SON STRING, PODEMOS LIMITAR EL ACCESO DE FORMA ALFABETICA 
+            //A LOS DATOS, YA SEA POR ROW KEY, PARTITION KEY O LOS DOS
+            builder.PartitionKeyStart = curso;
+            builder.PartitionKeyEnd = curso;
+
+            //CON ESTO YA PODEMOS GENERAR EL TOKEN QUE ES UN ACCESO POR URI
+            Uri uriToken = this.tablaAlumnos.GenerateSasUri(builder);
+            string token = uriToken.AbsoluteUri;
+            return token;
+        }
+    }
+}
